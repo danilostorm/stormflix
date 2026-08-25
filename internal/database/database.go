13eea59edc3e6092c7489e8cb77604e81e942a52
DaNiLoStOrM <storm.danilo@gmail.com>
2026-08-25 15:44:03 -0300

    phase2: run extended database migrations

--- internal/database/database.go
+++ internal/database/database.go
@@ -41,6 +41,10 @@ func Open(path string) (*sql.DB, error) {
 		db.Close()
 		return nil, err
 	}
+	if err := migratePhase2(db); err != nil {
+		db.Close()
+		return nil, err
+	}
 	return db, nil
 }
 
